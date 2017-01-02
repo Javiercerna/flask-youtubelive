ece3e163258b9f6e21c728f1ee7b922314593ba1
Javiercerna <javier.cerna@pucp.pe>
2017-01-02 09:00:46 -0500

    Add basic OAuth2 authentication * First try with youtube read, since it doesn't require https * Create oauth2 route * Store oauth2 credentials in credentials.py

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from flask import url_for, redirect, render_template, \
                      request, session, abort, jsonify
-
+from live_streaming_api import requestAccessToken
 from project import app, db
 from project.models import User
 import os
@@ -9,4 +9,15 @@ import json
 @app.route('/')
 def mainInterface():
     return 'Hello World'
+
+@app.route('/oauth2callback/')
+def accessYoutubeAccount():
+    if ('code' not in request.args):
+        auth_uri = requestAccessToken(url_for('accessYoutubeAccount',
+                                              _external=True))
+        return redirect(auth_uri)
+    else:
+        auth_code = request.args.get('code')
+        template = 'Signed in<br> Code: ' + auth_code
+        return template
 
